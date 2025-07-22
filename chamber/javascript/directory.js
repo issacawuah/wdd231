0c3411adb3b2438b87a78e14cce87b26279bdf49
issacawuah <email>
2025-07-22 18:06:14 +0000

    imaage fix

--- chamber/javascript/directory.js
+++ chamber/javascript/directory.js
@@ -53,24 +53,24 @@ const members = [
     membershipLevel: "Gold"
   },
   {
-    name: "Kingsmotion",
-    logo: "images/motion.jpg",
-    phone: "(223) 555-5678",
-    address: "20 Avenue de la Liberté, ",
+    name: "Ikona Health",
+    logo: "images/ikona.jpg",
+    phone: "(233)266838889",
+    address: "20 Avenue, amasaman road, ",
     website: "https://saharatraders.com",
     membershipLevel: "Silver"
   },
   {
-    name: "Mali Textiles",
-    logo: "images/images/motion.jpg",
-    phone: "(223) 555-9012",
+    name: "Kingsmotion",
+    logo: "images/motion.jpg",
+    phone: "(233) 450701235",
     address: "5 Rue des Artisans, ",
     website: "https://malitextiles.com",
     membershipLevel: "Gold"
   },
   {
     name: "Desert Imports",
-    logo: "",
+    logo: "images/motion.jpg",
     phone: "(223) 555-3456",
     address: "30 Boulevard Central",
     website: "https://desertimports.com",
